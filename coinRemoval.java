a4729673769538037814779a9bf126c2af0529ff
Kishorepm <kishorepmofficial@gmail.com>
2016-08-31 17:31:12 +0530

    Update coinRemoval.java

--- coinRemoval.java
+++ coinRemoval.java
@@ -4,11 +4,29 @@ public class coinRemoval {
 	public static void main(String args[])
 	{
 		char[][] matrix = {{'T', 'H', 'H'},
-				           {'T', 'H', 'T'},
-				           {'T', 'H', 'H'}};
+				   {'H', 'H', 'T'},
+		                   {'T', 'T', 'H'}};
 		
 		try
 		{
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				if(matrix[i][j]=='H')
+				{
+					matrix[i][j]='_';
+					if(matrix[i+1][j]=='T')
+					{
+						matrix[i+1][j]='H';
+						if(matrix[i+2][j]=='T')
+						{
+							matrix[i+2][j]='H';	
+						}
+					}
+				}
+			}
+		}
 		for(int i=0;i<3;i++)
 		{
 			for(int j=0;j<3;j++)
