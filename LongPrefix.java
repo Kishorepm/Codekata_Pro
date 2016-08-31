1c73588665d071bcce092f3fd70610fd6cd875fc
Kishorepm <kishorepmofficial@gmail.com>
2016-08-31 17:25:39 +0530

    Update LongPrefix.java

--- LongPrefix.java
+++ LongPrefix.java
@@ -1,5 +1,4 @@
 package pro_LEVEL;
-
 import java.util.*;
 public class LongPrefix
 {
@@ -8,12 +7,12 @@ public static void main(String ar[])throws Exception
 Scanner sc=new Scanner(System.in);
 int n,j;
 n=sc.nextInt();
-String a[]=new String[100];
+String a[]=new String[500];
 for(int i=0;i<n;i++)
 {
 a[i]=sc.next();
 }
-String lng = "",check;
+String longprifix = "",check;
 if(a.length>0)
 lng=a[0];
 for(int i=1;i<a.length;i++)
@@ -21,11 +20,11 @@ for(int i=1;i<a.length;i++)
 check=a[i];
 for(j=0;j<Math.min(lng.length(),a[i].length());j++)
 {
-if(lng.charAt(j)!=check.charAt(j))
+if(longprifix.charAt(j)!=check.charAt(j))
 break;
 }
-lng=a[i].substring(0,j);
+longprefix=a[i].substring(0,j);
 }
-System.out.println("longest prefix is:"+lng);
+System.out.println("longest prefix is:"+longprefix);
 }
 }
