db8ee218b0e3d142208a2ed89a48d6b5f1e1c39c
Kishorepm <kishorepmofficial@gmail.com>
2016-08-31 17:35:58 +0530

    Update LongPrefix.java

--- LongPrefix.java
+++ LongPrefix.java
@@ -4,27 +4,27 @@ public class LongPrefix
 {
 public static void main(String ar[])throws Exception
 {
-Scanner sc=new Scanner(System.in);
-int n,j;
-n=sc.nextInt();
-String a[]=new String[500];
-for(int i=0;i<n;i++)
-{
-a[i]=sc.next();
-}
-String longprifix = "",check;
-if(a.length>0)
-lng=a[0];
-for(int i=1;i<a.length;i++)
-{
-check=a[i];
-for(j=0;j<Math.min(lng.length(),a[i].length());j++)
-{
-if(longprifix.charAt(j)!=check.charAt(j))
-break;
-}
-longprefix=a[i].substring(0,j);
-}
-System.out.println("longest prefix is:"+longprefix);
-}
+  Scanner s=new Scanner(System.in);
+  int n,j;
+  n=s.nextInt();
+  String a[]=new String[500];
+  for(int i=0;i<n;i++)
+    {
+      a[i]=s.next();
+    }
+  String longprifix = "",check;
+  if(a.length>0)
+    longprifix=a[0];
+   for(int i=1;i<a.length;i++)
+   {
+      check=a[i];
+      for(j=0;j<Math.min(lng.length(),a[i].length());j++)
+         {
+          if(longprifix.charAt(j)!=check.charAt(j))
+          break;
+          }
+        longprefix=a[i].substring(0,j);
+    }
+  System.out.println("longest prefix is:"+longprefix);
+  }
 }
